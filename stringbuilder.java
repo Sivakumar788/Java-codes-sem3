758166d4a5850f466db10269a5cf31e5cdfcb41d
Sivakumar788 <142193843+BazingaCooper2@users.noreply.github.com>
2024-08-03 20:26:51 +0530

    second commit

---
+++ stringbuilder.java
@@ -0,0 +1,8 @@
+public class stringbuilder {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder("hello");
+        sb.append("java");
+        System.out.println(sb);
+    }
+
+}
